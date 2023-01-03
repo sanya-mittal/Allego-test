9a30a5046e5616ae09f5b7c8c5bf99046f979a2b
sanyamittal <sanyamittal@localhost>
2023-01-03 11:53:24 +0530

    COMMIT 1

---
+++ Test Suites/Allego test.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Allego test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>2877e194-14b4-4689-91ff-2d0aef9a272b</testSuiteGuid>
+   <testCaseLink>
+      <guid>ce8e9ff0-75b7-483c-bf01-1991036f0bb1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Demo record and playback</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
